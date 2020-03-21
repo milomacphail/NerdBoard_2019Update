f51a8c331c7ba409733ca7c2687df62b03222617
milomacphail <milomacphail@gmail.com>
2020-03-21 18:17:30 -0400

    Server running

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
