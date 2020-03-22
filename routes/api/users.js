a79f5592c9f1a1f743ec271d83c56b6195e1f197
milomacphail <milomacphail@gmail.com>
2020-03-22 10:58:11 -0400

    Express validation added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 //route GET
-//desc TEST route
+//desc register user
 //access PUBLIC
-router.get('/', (req, res) => res.send('User connected'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please provide a valid email').isEmail(),
+    check('password', 'Passwords must be six or more characters').isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    res.send('User connected');
+  }
+);
 
 //GET
 //desc GET USERS
