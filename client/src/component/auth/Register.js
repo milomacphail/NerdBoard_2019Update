292ebf3ce0d64cc1e10cea1322fd957052db613a
milomacphail <milomacphail@gmail.com>
2020-03-29 13:25:06 -0400

    Login alert set

--- client/src/component/auth/Register.js
+++ client/src/component/auth/Register.js
@@ -1,8 +1,12 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
+
 //import axios from 'axios';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -16,7 +20,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Passwords don't match.");
+      setAlert('Passwords do not match', 'danger');
     } else {
       console.log('Success');
     }
@@ -92,4 +96,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register);
