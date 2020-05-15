0c75097d0b75522935ed0b44cf2c0e37e0857474
milomacphail <milomacphail@gmail.com>
2020-05-15 18:48:25 -0400

    Create profile page finished

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,9 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './component/layout/Navbar';
 import Landing from './component/layout/Landing';
-import Login from './component/auth/Login';
-import Register from './component/auth/Register';
-import Alert from './component/layout/Alert';
-import Dashboard from './component/dashboard/Dashboard';
-import PrivateRoute from './component/routing/PrivateRoute';
+import Routes from './component/routing/Routes';
 
-//Redux
+// Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
@@ -16,31 +12,25 @@ import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
-if (localStorage.token) {
-  setAuthToken(localStorage.token);
-}
-
 const App = () => {
   useEffect(() => {
+    setAuthToken(localStorage.token);
     store.dispatch(loadUser());
   }, []);
+
   return (
     <Provider store={store}>
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path="/" component={Landing} />
-          <section className="container">
-            <Alert />
-            <Switch>
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
-            </Switch>
-          </section>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
   );
 };
+
 export default App;
